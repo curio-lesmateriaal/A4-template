7bd3870afd32151f915a648a03dd44c8468a3351
e.boekhoudt <askanix@users.noreply.github.com>
2022-01-11 08:24:04 +0100

    foto robin

--- includes/collegas/robin.php
+++ includes/collegas/robin.php
@@ -1,5 +1,5 @@
 <div class="collega">
-	<img src="img/profile_pictures/temp.png" alt="rl20">
+	<img src="img/profile_pictures/robin.jfif" alt="rl20">
 	<h4>Robin Louwerse</h4>
 	<p>docent PRA</p>
 	<p>r.louwerse@curio.nl</p>
