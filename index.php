82a4b6d3936e08787bc14b75de0b06e10f60c571
e.boekhoudt <askanix@users.noreply.github.com>
2023-05-30 10:19:15 +0200

    Update index.php
    update font awesome link. icons in footer weren't showing

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
         <link rel="stylesheet" href="css/main.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Stencil+Display:wght@700&family=Inconsolata&display=swap" rel="stylesheet">
-
-        <script src="https://kit.fontawesome.com/43363efb2f.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+     
     </head>
 
     <body>
